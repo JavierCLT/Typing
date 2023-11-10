504265c1fb084f9578403c85a98d7bf790c24e24
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2023-11-10 14:44:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -206,19 +206,10 @@ document.addEventListener('DOMContentLoaded', () => {
   container.classList.add('fade-in');
 
   const wordInput = document.getElementById('wordInput');
-  // Set placeholder text for the input box
-  wordInput.placeholder = 'Press Enter to Start';
   wordInput.addEventListener('input', handleKeyPress);
-  
-  // Event listener for Enter key to start the game
-  wordInput.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter' && !isGameStarted) {
-      // Remove placeholder text
-      wordInput.placeholder = '';
-      // Start the game
-      isGameStarted = true;
-      wordInput.focus(); // Focus the input field
-      setNewWord(); // Set the first word and play the sound
-    }
-  });
+
+  // Automatically start the game without the need for pressing Enter
+  isGameStarted = true;
+  wordInput.focus(); // Focus the input field automatically
+  setNewWord(); // Set the first word and play the sound
 });
